45033461b4bf08035f73682ea9e105f94555076f
jihoLee0818 <leejiho0114@gmail.com>
2025-07-30 20:34:31 +0900

    feat: homework2 최초등록

---
+++ homework2/project-name/src/main/java/org/example/projectname/entity/Member.java
@@ -0,0 +1,24 @@
+package org.example.projectname.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Member(String name) {
+        this.name = name;
+    }
+}
